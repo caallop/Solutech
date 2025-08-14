203035eabcf8df0503d1467b9b17171b92181b24
Guilherme <guilopes0606@hmail.com>
2025-08-13 21:45:35 -0300

    botao para trocar de pagina

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -13,3 +13,59 @@ let desiredDeadline = document.getElementById('desiredDeadline')
 let attachments = document.getElementById('attachments')
 let submitButton = document.getElementById('submitButton')
 
+
+
+
+/**
+ * 
+ * 
+ */
+
+let companyName = document.getElementById('companyName')
+let contactName = document.getElementById('contactName')
+let email = document.getElementById('email')
+let password = document.getElementById('password')
+let phone = document.getElementById('phone')
+let cnpjCompany = document.getElementById('cnpjCompany')
+
+clientForm.addEventListener('submit', (e) => {
+    /*e.preventDefault()
+    console.log("teste do botao")
+    console.log("teste do botao")
+    console.log("teste do botao")
+    console.log(phone);
+    console.log(contactName);
+    console.log(companyName);
+    console.log(email);
+    console.log(password);
+    console.log(cnpjCompany)
+    alert(phone);
+    alert(contactName);
+    alert(companyName);
+    alert(email);
+    alert(password);
+    alert(cnpjCompany)*/
+})
+
+
+async function requestProject() {
+    console.log("teste do botao")
+    console.log("teste do botao")
+    console.log("teste do botao")
+    alert(`  Empresa: ${clientId.value}
+             Contato: ${projectName.value}
+             Email ${projectDescription.value}
+             Senha ${projectType.value}
+             Telefone ${desiredDeadline.value}
+             CNPJ ${cnpjCompany.value}`);
+    console.log(`  Empresa: ${attachments.value}`)
+
+    /* const clientData = {
+         companyName: companyName,
+         contactName: contactName,
+         email: email,
+         password: password,
+         phone: phone,
+         cnpjCompany: cnpjCompany
+     };*/
+}
